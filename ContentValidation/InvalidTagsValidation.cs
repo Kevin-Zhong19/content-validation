b7941b787adb4e1d9a0d70606ced9957c322a4a1
Qishun Zhong (WICRESOFT NORTH AMERICA LTD) <v-qzhong@microsoft.com>
2025-02-25 14:21:16 +0800

    rename InvalidTagsValidation

--- ContentValidation/InvalidTagsValidation.cs
+++ ContentValidation/InvalidTagsValidation.cs
@@ -2,11 +2,11 @@ using Microsoft.Playwright;
 
 namespace UtilityLibraries;
 
-public class MethodTypeAnnotationValidation : IValidation
+public class InvalidTagsValidation : IValidation
 {
     private IPlaywright _playwright;
 
-    public MethodTypeAnnotationValidation(IPlaywright playwright)
+    public InvalidTagsValidation(IPlaywright playwright)
     {
         _playwright = playwright;
     }
@@ -27,7 +27,7 @@ public class MethodTypeAnnotationValidation : IValidation
             // List of valid HTML tags
             "a", "abbr", "acronym", "address", "applet", "area", "article", "aside", "audio", "b", "base", "basefont", "bdi", "bdo", "big", "blockquote", "body", "br", "button", "canvas", "caption", "center", "cite", "code", "col", "colgroup", "data", "datalist", "dd", "del", "details", "dfn", "dialog", "dir", "div", "dl", "dt", "em", "embed", "fieldset", "figcaption", "figure", "font", "footer", "form", "frame", "frameset", "h1", "h2", "h3", "h4", "h5", "h6", "head", "header", "hgroup", "hr", "html", "i", "iframe", "img", "input", "ins", "kbd", "keygen", "label", "legend", "li", "link", "main", "map", "mark", "menu", "menuitem", "meta", "meter", "nav", "noframes", "noscript", "object", "ol", "optgroup", "option", "output", "p", "param", "picture", "pre", "progress", "q", "rp", "rt", "ruby", "s", "samp", "script", "section", "select", "small", "source", "span", "strike", "strong", "style", "sub", "summary", "sup", "table", "tbody", "td", "textarea", "tfoot", "th", "thead", "time", "title", "tr", "track", "tt", "u", "ul", "var", "video", "wbr"
         };
-        
+
         var tagNames = await page.EvaluateAsync<string[]>("() => Array.from(document.querySelectorAll('body *'), el => el.tagName.toLowerCase())");
 
         foreach (var tagName in tagNames)
@@ -41,7 +41,7 @@ public class MethodTypeAnnotationValidation : IValidation
             res.Result = false;
             res.ErrorLink = testLink;
             res.NumberOfOccurrences = invalidTags.Count;
-            res.ErrorInfo = "Missing Method Type Annotation: " + string.Join(",", allTags);
+            res.ErrorInfo = "Invalid tags found: " + string.Join(",", allTags);
         }
 
         await browser.CloseAsync();
