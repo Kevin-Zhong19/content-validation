eee223a08194c90d7f2734719d058294396382b2
v-qzhong <v-qzhong@microsoft.com>
2025-07-10 09:57:20 +0800

    change branch

--- ContentValidation.Test/TestPageContent.cs
+++ ContentValidation.Test/TestPageContent.cs
@@ -66,7 +66,7 @@ namespace ContentValidation.Test
         [Category("PythonTest")]
         [Category("JavaTest")]
         [Category("JsTest")]
-        [Category("DotNetTest")]
+        // [Category("DotNetTest")]
         [TestCaseSource(nameof(TestLinks))]
         public async Task TestTableMissingContent(string testLink)
         {
@@ -180,7 +180,7 @@ namespace ContentValidation.Test
 
 
         [Test]
-        // [Category("DotNetTest")]
+        [Category("DotNetTest")]
         [TestCaseSource(nameof(TestLinks))]
         public async Task TestEmptyTags(string testLink)
         {
